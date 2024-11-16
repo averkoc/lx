0e831d903736390f15f6cd262fc59a3f32051fd4
averkoc <sakari.mannisto@centria.fi>
2024-11-16 12:18:03 +0200

    Update mqttdemo2.py

--- mqttdemo2.py
+++ mqttdemo2.py
@@ -8,7 +8,9 @@ import random
 broker = "broker.emqx.io"
 port = 1883
 
-topic = f"/linuxessentials/sakari/temperaturevalue"
+topictemp = f"/linuxessentials/sakari/temperatur"
+topicpressure = f"/linuxessentials/sakari/pressure"
+
 
 client = mqtt.Client()
 
@@ -17,6 +19,9 @@ def publish_temperature():
         temperature = random.uniform(20.0, 30.0)
         client.publish(topic, f"{temperature:.2f}")
         print(f"Published: {temperature:.2f} to topic {topic}")
+        pressure = random.uniform(1020.0, 1030.0)
+        client.publish(topic, f"{pressure:.2f}")
+        
         time.sleep(5)
 
 client.connect(broker, port)
